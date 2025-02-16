ca024f6c245328968114913bb281337d3746e1b0
MAlvar000 <alvaro.mateocarrero@usp.ceu.es>
2025-02-16 19:42:48 +0100

    Ejercicio1

--- aplicacion/principal.java
+++ aplicacion/principal.java
@@ -1,24 +1,24 @@
 package aplicacion;
 
-import mates.Numeros;
+import mates.Solucion;
 
 public class principal {
     public static void main(String[] args){
         int n = 30;
-        Numeros.suma(n);
-        Numeros.factorial(n);
-        Numeros.potencia(8,6);
+        Solucion.suma(n);
+        Solucion.factorial(n);
+        Solucion.potencia(8,6);
         int[] listaNumeros = {1, 2, 3, 4, 5};
-        Numeros.sumaLista(listaNumeros);
-        Numeros.mediaAritmetica(listaNumeros);
-        Numeros.desviacionTipica(listaNumeros);
-        Numeros.sumaPares(n);
+        Solucion.sumaLista(listaNumeros);
+        Solucion.mediaAritmetica(listaNumeros);
+        Solucion.desviacionTipica(listaNumeros);
+        Solucion.sumaPares(n);
         int[] listaEnteros = {2, 5, 8, 11, 14};
-        Numeros.sumaElementosPares(listaEnteros);
-        Numeros.obtenerListaPar(listaEnteros);
-        Numeros.listaPar(n);
+        Solucion.sumaElementosPares(listaEnteros);
+        Solucion.obtenerListaPar(listaEnteros);
+        Solucion.listaPar(n);
         int[] listaA = {1, 3, 5};
         int[] listaB = {2, 4, 6};
-        Numeros.calcularProductoEscalar(listaA, listaB);
+        Solucion.calcularProductoEscalar(listaA, listaB);
     }
 }
